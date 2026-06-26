b7142dbce588b5bc4d2c1d79894dfe6e1515012a
PuneetSharma-1 <PuneetSharmax01@gmail.com>
2026-06-27 04:06:10 +0530

    Add HelloWorld, Variables, and DataTypes programs

---
+++ 01-Fundamentals/B_Variables.java
@@ -0,0 +1,13 @@
+
+public class B_Variables {
+
+   static String name="Puneet";
+   static int age=20;
+
+    public static void main(String[]args)
+    {
+        System.out.println(name);
+        System.out.println(age);
+    }
+}
+
---
+++ 01-Fundamentals/C_DataTypes.java
@@ -0,0 +1,12 @@
+public class C_DataTypes {
+
+    static int num1=10;
+    static double num2=20.0;
+
+    public static void main(String[]args)
+    {
+     System.out.println(num1);
+     System.out.println(num2);
+    }
+    
+}

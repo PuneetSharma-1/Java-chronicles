48ab1185f234e79b81d2ee6bd380a65c3084b5c6
PuneetSharma-1 <PuneetSharmax01@gmail.com>
2026-06-28 16:52:04 +0530

    Added Java Typecasting and Operators program

---
+++ 01-Fundamentals/D_Typecasting.java
@@ -0,0 +1,8 @@
+public class D_Typecasting {
+    static int i=96;
+   static  double a= i/10.0;
+    public static void main(String[]args)
+    {
+      System.out.println(a);
+    }
+}
---
+++ 01-Fundamentals/E_Operators.java
@@ -0,0 +1,10 @@
+
+public class E_Operators {
+    static int num1=2,num2=5;
+
+    public static void main(String[]args)
+    {
+        System.out.println(num1+num2);
+    }
+    
+}

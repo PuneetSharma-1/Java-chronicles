ae7ec4a98d0a5918e4b379ab39ba7b9b63cfd0e1
PuneetSharma-1 <PuneetSharmax01@gmail.com>
2026-06-29 16:33:58 +0530

    Added Comments and Identifier's Program

---
+++ 01-Fundamentals/Comments.java
@@ -0,0 +1,13 @@
+public class Comments{
+    //Class name Comments
+    public static void main(String[]args)
+    {
+        /*Now in this numch we can write anything and 
+        it will not interupt our code, 
+        That is what comments
+         */
+        System.out.println("Comments Added");
+    }
+
+}
+    
---
+++ 01-Fundamentals/Identifiers.java
@@ -0,0 +1,11 @@
+public class Identifiers {
+   static int age=12;
+   static String name="Puneet";
+   static int marks = 75;
+   public static void main(String []args)
+   {
+     System.out.println(age);
+     System.out.println(name);
+     System.out.println(marks);
+   }  
+}

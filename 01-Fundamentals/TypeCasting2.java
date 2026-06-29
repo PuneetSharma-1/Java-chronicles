80e8d2d678fcfe13aa81b10027fa33e5e4832c0d
PuneetSharma-1 <PuneetSharmax01@gmail.com>
2026-06-30 04:06:58 +0530

    Implement ASCII to Character conversion logic

---
+++ 01-Fundamentals/TypeCasting2.java
@@ -0,0 +1,10 @@
+//This is second example of typecasting
+public class TypeCasting2 {
+    static char C=97;
+    public static void main(String[]args)
+    {
+        System.out.println(C);
+        //we will get the ASCII CODE of that given number
+    }
+    
+}

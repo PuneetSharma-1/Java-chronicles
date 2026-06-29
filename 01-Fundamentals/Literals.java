643e1c6c7b9f9454c6582c12f3fcd89873b83655
PuneetSharma-1 <PuneetSharmax01@gmail.com>
2026-06-29 16:44:39 +0530

    Added Constants and Literals Program

---
+++ 01-Fundamentals/Literals.java
@@ -0,0 +1,16 @@
+public class Literals {
+
+    static int age=17;
+    static int standard=9;
+    static String name="Anmol Roy";
+    static double percentage= 92;
+    public static void main (String[]args)
+    {
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(standard);
+        System.out.println(percentage);
+    
+    }
+    
+}
